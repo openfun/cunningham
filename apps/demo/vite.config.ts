f635c080601c477160b0c7f0c43df5ba6f4e4913
Nathan Vasse <nathan.vasse@gmail.com>
2022-12-01 12:05:50 +0100

    ✨(demo) add app demo
    This demo is useful to test in the real usage of the design system as a real
    user that will generate its own customized design tokens, implements the design
    system's components, etc ...

---
+++ apps/demo/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 3200,
+    hmr: {
+      port: 3201,
+    },
+  },
+});
