54f1dd94dde8e7134c1dad3e2f0c2c64c707b20c
Nathan Vasse <nathan.vasse@gmail.com>
2022-12-01 12:05:22 +0100

    ✨(frontend) add storybook to react repo
    Add storybook for the react repo, this will make developing components
    enjoyable ! It can also serve as a single source of thruth.

---
+++ packages/react/.storybook/main.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    '@storybook/preset-scss'
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-vite"
+  },
+  staticDirs: [
+    'dist',
+  ],
+  "features": {
+    "storyStoreV7": true
+  }
+}
