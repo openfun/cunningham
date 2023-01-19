82bb88f5be75f880e289cc6fa5f8b456f519e585
Nathan Vasse <nathan.vasse@gmail.com>
2023-01-19 15:31:22 +0100

    📝(doc) create a global category for Components
    This way we ensure that the main categories of the Storybook docs
    are well separated, improving readability.

--- packages/react/src/components/Button/index.stories.tsx
+++ packages/react/src/components/Button/index.stories.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Button } from "./index";
 
 export default {
-  title: "Button",
+  title: "Components/Button",
   component: Button,
 } as ComponentMeta<typeof Button>;
 
