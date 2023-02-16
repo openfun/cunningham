980c80c7847b7476fc797d8d2a308cbd3c43827d
Nathan Vasse <nathan.vasse@gmail.com>
2023-02-16 14:42:18 +0100

    ➕(react) add material icons
    We need some icons to implement the Pagination and the DataGrid.

---
+++ packages/react/vite.icons.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vitest/config";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    emptyOutDir: false,
+    outDir: "dist",
+    lib: {
+      entry: {
+        icons: "./src/icons.scss",
+      },
+      formats: ["es"],
+    },
+    rollupOptions: {
+      input: "src/icons.scss",
+      output: {
+        assetFileNames: "icons.css",
+      },
+    },
+  },
+});
