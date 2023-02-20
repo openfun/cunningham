88e478d9f6f3a74277b2be3eae8ac8a00e263c2d
Nathan Vasse <nathan.vasse@gmail.com>
2023-02-20 16:27:37 +0100

    ✨(react) add Pagination component
    In order to create a DataGrid we first need a fully working pagination
    component. It comes with multiples working examples in the documentation.

---
+++ packages/react/src/components/Pagination/index.stories.tsx
@@ -0,0 +1,84 @@
+import { ComponentMeta } from "@storybook/react";
+import React, { useEffect, useMemo, useState } from "react";
+import { Pagination, usePagination } from "components/Pagination/index";
+import { CunninghamProvider } from "components/Provider";
+
+export default {
+  title: "Components/Pagination",
+  component: Pagination,
+} as ComponentMeta<typeof Pagination>;
+
+export const Basic = () => {
+  const pagination = usePagination({
+    defaultPagesCount: 100,
+    defaultPage: 50,
+  });
+  return (
+    <CunninghamProvider>
+      <Pagination {...pagination} />
+    </CunninghamProvider>
+  );
+};
+
+export const List = () => {
+  // Numbers from 0 to 99.
+  const database = useMemo(() => Array.from(Array(100).keys()), []);
+  // Items to display on the current page.
+  const [items, setItems] = useState<number[]>([]);
+  const pagination = usePagination({ pageSize: 10 });
+
+  // On page change.
+  useEffect(() => {
+    // Simulate a HTTP request delay.
+    const timeout = setTimeout(() => {
+      // Sets the number of pages based on the number of items in the database.
+      pagination.setPagesCount(
+        Math.ceil(database.length / pagination.pageSize)
+      );
+      // Sets the items to display on the current page.
+      setItems(
+        database.slice(
+          (pagination.page - 1) * pagination.pageSize,
+          pagination.page * pagination.pageSize
+        )
+      );
+    }, 500);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [pagination.page]);
+
+  return (
+    <CunninghamProvider>
+      <div>
+        <div>
+          {items.map((item) => (
+            <div className="p-t bg-secondary-300 mb-t" key={item}>
+              {item}
+            </div>
+          ))}
+        </div>
+        <Pagination {...pagination} />
+      </div>
+    </CunninghamProvider>
+  );
+};
+
+export const ForcePage = () => {
+  const pagination = usePagination({
+    defaultPagesCount: 10,
+  });
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      pagination.setPage(5);
+    }, 500);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+  return (
+    <CunninghamProvider>
+      <Pagination {...pagination} />
+    </CunninghamProvider>
+  );
+};
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,4 +1,5 @@
 import "./index.scss";
 
 export * from "./components/Button";
+export * from "./components/Pagination";
 export * from "./components/Provider";
