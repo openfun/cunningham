72091eecbcf85a4f4aa8dc2aa4ab3910f59004bb
jbpenrath <jb.penrath@gmail.com>
2023-04-17 16:50:46 +0200

    🩹(tokens) add blank line at end of token files
    Cunningham CLI did not add a blank line at the end of generated
    token files.

---
+++ .changeset/rotten-plums-leave.md
@@ -0,0 +1,5 @@
+---
+"@openfun/cunningham-tokens": patch
+---
+
+Add a blank line at the end of token files
--- packages/tokens/src/bin/Generators/TsGenerator.spec.ts
+++ packages/tokens/src/bin/Generators/TsGenerator.spec.ts
@@ -23,8 +23,9 @@ describe("TsGenerator", () => {
     expect(fs.existsSync(tokensFile)).toEqual(false);
     await run(["", "", "-g", "ts"]);
     expect(fs.existsSync(tokensFile)).toEqual(true);
-    expect(fs.readFileSync(tokensFile).toString()).toEqual(
-      `export const tokens = {"theme":{"colors":{"primary":"#055FD2","secondary":"#DA0000"},"font":{"sizes":{"m":"1rem"},"weights":{"medium":400},"families":{"base":"Roboto"}},"spacings":{"s":"1rem"}}};`
-    );
+    expect(fs.readFileSync(tokensFile).toString()).toMatchInlineSnapshot(`
+      "export const tokens = {"theme":{"colors":{"primary":"#055FD2","secondary":"#DA0000"},"font":{"sizes":{"m":"1rem"},"weights":{"medium":400},"families":{"base":"Roboto"}},"spacings":{"s":"1rem"},"transitions":{"ease":"linear"}}};
+      "
+    `);
   });
 });
