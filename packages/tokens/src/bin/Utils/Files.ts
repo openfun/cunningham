9320bc6b72750387c873207eb8665419387982a0
jbpenrath <jb.penrath@gmail.com>
2023-04-17 16:50:46 +0200

    🩹(tokens) add blank line at end of token files
    Cunningham CLI did not add a blank line at the end of generated
    token files.

--- packages/tokens/src/bin/Utils/Files.ts
+++ packages/tokens/src/bin/Utils/Files.ts
@@ -8,6 +8,10 @@ export const put = (path: string, content: string) => {
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir);
   }
+
+  // Add a new line at the end of the file
+  content += "\n";
+
   fs.writeFileSync(path, content);
   console.log(
     chalk.bgGreen(chalk.white("File " + path + " generated successfully."))
