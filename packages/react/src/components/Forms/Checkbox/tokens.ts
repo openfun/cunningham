7b4cfbbf9470145b19e8f7346ae67e2aa4953d6f
Nathan Vasse <nathan.vasse@gmail.com>
2023-05-17 15:18:46 +0200

    💄(react) make inputs labels bolder
    The current labels of radio, checkbox and switches were not bold enough
    to match the figma sketches.

--- packages/react/src/components/Forms/Checkbox/tokens.ts
+++ packages/react/src/components/Forms/Checkbox/tokens.ts
@@ -3,7 +3,7 @@ import { DefaultTokens } from "@openfun/cunningham-tokens";
 export const tokens = (defaults: DefaultTokens) => ({
   "background-color--hover": defaults.theme.colors["greyscale-200"],
   "font-size": defaults.theme.font.sizes.m,
-  "font-weight": defaults.theme.font.weights.medium,
+  "font-weight": defaults.theme.font.weights.bold,
   color: defaults.theme.colors["greyscale-900"],
   "border-color": defaults.theme.colors["greyscale-300"],
   "border-radius": "2px",
