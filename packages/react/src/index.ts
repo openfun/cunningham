1944a905d4ef248a051b16f2f68efc642e8d4bdd
Nathan Vasse <nathan.vasse@gmail.com>
2023-05-17 15:21:46 +0200

    ✨(react) add Switch component
    Implement a shiny new component that can be used as an alternative to
    the regular checkbox.

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -11,6 +11,7 @@ export * from "./components/Forms/Field";
 export * from "./components/Forms/Input";
 export * from "./components/Forms/Radio";
 export * from "./components/Forms/Select";
+export * from "./components/Forms/Switch";
 export * from "./components/Loader";
 export * from "./components/Pagination";
 export * from "./components/Provider";
