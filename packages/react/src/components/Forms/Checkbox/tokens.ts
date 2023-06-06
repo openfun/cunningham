b010089b807e6bdc1cd402820b29935e79b8a5f9
Lebaud Antoine <lebaud.antoine131@gmail.com>
2023-06-06 10:07:07 +0200

    ♻️(react) improve checkbox styling configuration
    Align design tokens of checkbox component with select component's
    design tokens for consistent styling configuration.

--- packages/react/src/components/Forms/Checkbox/tokens.ts
+++ packages/react/src/components/Forms/Checkbox/tokens.ts
@@ -2,6 +2,7 @@ import { DefaultTokens } from "@openfun/cunningham-tokens";
 
 export const tokens = (defaults: DefaultTokens) => ({
   "background-color--hover": defaults.theme.colors["greyscale-200"],
+  "background-color": "white",
   "font-size": defaults.theme.font.sizes.m,
   "font-weight": defaults.theme.font.weights.medium,
   color: defaults.theme.colors["greyscale-900"],
