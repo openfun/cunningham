2130e967118aaf1b8a168544acac99916c37afc6
Lebaud Antoine <lebaud.antoine131@gmail.com>
2023-06-06 10:33:16 +0200

    💄(react) improve radio input styling configuration
    Align design tokens of radio input component with select component's
    design tokens for consistent styling configuration.

--- packages/react/src/components/Forms/Radio/tokens.ts
+++ packages/react/src/components/Forms/Radio/tokens.ts
@@ -3,4 +3,5 @@ import { DefaultTokens } from "@openfun/cunningham-tokens";
 export const tokens = (defaults: DefaultTokens) => ({
   "border-color": defaults.theme.colors["greyscale-300"],
   "accent-color": defaults.theme.colors["success-700"],
+  "background-color": "white",
 });
