c49d98499b3f31ca0455ead4b168bcd33bfe507f
Nathan Vasse <nathan.vasse@gmail.com>
2023-06-22 17:15:47 +0200

    ✨(react) improve loader
    Add small size to the loader for the file uploader.

--- packages/react/src/components/Loader/index.tsx
+++ packages/react/src/components/Loader/index.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import classNames from "classnames";
 
 interface Props {
   "aria-label"?: string;
+  size?: "small" | "medium";
 }
 
-export const Loader = (props: Props) => {
-  return <div className="c__loader" role="status" {...props} />;
+export const Loader = ({ size = "medium", ...props }: Props) => {
+  return (
+    <div
+      className={classNames("c__loader", "c__loader--" + size)}
+      role="status"
+      {...props}
+    />
+  );
 };
