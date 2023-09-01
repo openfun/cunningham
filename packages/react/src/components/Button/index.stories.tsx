c0028966f17682782f5e6a71bfeb070b40171e8a
Nathan Vasse <nathan.vasse@gmail.com>
2023-09-01 17:33:15 +0200

    ✨(react) add a nano button, proper icon sizes
    Previously we had to hack the button in order to display small clickable
    icons, like the clear button in the DatePicker. By introducing a new
    nano button we can natively achieve that. Also make more homegenous
    font size and icon sizes in existing button sizes.

--- packages/react/src/components/Button/index.stories.tsx
+++ packages/react/src/components/Button/index.stories.tsx
@@ -54,6 +54,14 @@ export const Small: Story = {
   },
 };
 
+export const Nano: Story = {
+  args: {
+    children: "Primary",
+    color: "primary",
+    size: "nano",
+  },
+};
+
 export const FullWidth: Story = {
   args: {
     children: "Primary",
@@ -67,44 +75,14 @@ export const FullWidthWithIcon: Story = {
     children: "Primary",
     color: "primary",
     fullWidth: true,
-    icon: (
-      <svg
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fill="currentColor"
-          fillRule="evenodd"
-          clipRule="evenodd"
-          d="M17.8724 10.0166H13.248L16.6251 4.07749C16.9258 3.54846 16.2447 3.01176 15.8005 3.42755L6.57343 12.0655C6.22192 12.3946 6.45489 12.9838 6.93615 12.9838H11.5606L8.18353 18.9229C7.88275 19.4519 8.56335 19.9886 9.00746 19.5728L18.2352 10.9349C18.5867 10.6058 18.3537 10.0166 17.8724 10.0166Z"
-        />
-      </svg>
-    ),
+    icon: <span className="material-icons">bolt</span>,
   },
 };
 
 export const IconLeft: Story = {
   args: {
     children: "Icon",
-    icon: (
-      <svg
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fill="currentColor"
-          fillRule="evenodd"
-          clipRule="evenodd"
-          d="M17.8724 10.0166H13.248L16.6251 4.07749C16.9258 3.54846 16.2447 3.01176 15.8005 3.42755L6.57343 12.0655C6.22192 12.3946 6.45489 12.9838 6.93615 12.9838H11.5606L8.18353 18.9229C7.88275 19.4519 8.56335 19.9886 9.00746 19.5728L18.2352 10.9349C18.5867 10.6058 18.3537 10.0166 17.8724 10.0166Z"
-        />
-      </svg>
-    ),
+    icon: <span className="material-icons">bolt</span>,
     color: "primary",
   },
 };
@@ -113,22 +91,7 @@ export const IconRight: Story = {
   args: {
     children: "Icon",
     iconPosition: "right",
-    icon: (
-      <svg
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fill="currentColor"
-          fillRule="evenodd"
-          clipRule="evenodd"
-          d="M17.8724 10.0166H13.248L16.6251 4.07749C16.9258 3.54846 16.2447 3.01176 15.8005 3.42755L6.57343 12.0655C6.22192 12.3946 6.45489 12.9838 6.93615 12.9838H11.5606L8.18353 18.9229C7.88275 19.4519 8.56335 19.9886 9.00746 19.5728L18.2352 10.9349C18.5867 10.6058 18.3537 10.0166 17.8724 10.0166Z"
-        />
-      </svg>
-    ),
+    icon: <span className="material-icons">bolt</span>,
     color: "primary",
   },
 };
@@ -136,22 +99,7 @@ export const IconRight: Story = {
 export const IconOnly: Story = {
   args: {
     "aria-label": "Button with only an icon",
-    icon: (
-      <svg
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fill="currentColor"
-          fillRule="evenodd"
-          clipRule="evenodd"
-          d="M17.8724 10.0166H13.248L16.6251 4.07749C16.9258 3.54846 16.2447 3.01176 15.8005 3.42755L6.57343 12.0655C6.22192 12.3946 6.45489 12.9838 6.93615 12.9838H11.5606L8.18353 18.9229C7.88275 19.4519 8.56335 19.9886 9.00746 19.5728L18.2352 10.9349C18.5867 10.6058 18.3537 10.0166 17.8724 10.0166Z"
-        />
-      </svg>
-    ),
+    icon: <span className="material-icons">bolt</span>,
     color: "primary",
   },
 };
