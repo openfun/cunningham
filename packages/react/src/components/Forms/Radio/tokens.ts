cdace69482e8bf04262658ef2f8b1bee6f5e685b
Nathan Vasse <nathan.vasse@gmail.com>
2023-09-07 14:30:43 +0200

    💄(react) adjust checkbox and radio borders and colors
    Based on a QA feedback.

--- packages/react/src/components/Forms/Radio/tokens.ts
+++ packages/react/src/components/Forms/Radio/tokens.ts
@@ -2,6 +2,6 @@ import { DefaultTokens } from "@openfun/cunningham-tokens";
 
 export const tokens = (defaults: DefaultTokens) => ({
   "border-color": defaults.theme.colors["greyscale-300"],
-  "accent-color": defaults.theme.colors["success-700"],
+  "accent-color": defaults.theme.colors["success-600"],
   "background-color": "white",
 });
