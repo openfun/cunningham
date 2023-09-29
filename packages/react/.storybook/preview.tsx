2e644b3c3a34747ac05d315d8c3125d4d20c6236
Nathan Vasse <nathan.vasse@gmail.com>
2023-09-29 16:53:13 +0200

    📝(doc) add migration guide
    As this new version introduced some breaking changes, this doc was needed.

--- packages/react/.storybook/preview.tsx
+++ packages/react/.storybook/preview.tsx
@@ -42,7 +42,7 @@ const preview: Preview = {
         },
         options: {
             storySort: (a, b) => {
-                const roots = ['Getting Started', 'Components'];
+                const roots = ['Getting Started', 'Components', 'Migrating'];
                 const gettingStartedOrder = [
                     'Installation',
                     'First steps',
