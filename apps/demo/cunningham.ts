b4be87961f3b7e6ee02afe61e134749db13d0258
Nathan Vasse <nathan.vasse@gmail.com>
2024-01-18 16:49:47 +0100

    ✨(demo) new demo
    This new demo aims to take advantage of all the new Cunningham's
    components. The old demo was kind of a draft, this new one gives
    a better overview of what Cunningham is capable of.

--- apps/demo/cunningham.ts
+++ apps/demo/cunningham.ts
@@ -1,14 +1,5 @@
 import { Configuration } from "@openfun/cunningham-react";
 
-const config: Configuration = {
-  themes: {
-    default: {
-      components: {
-        button: {
-          "border-radius": "30px",
-        },
-      },
-    },
-  },
-};
-export default config;
+const defaultConfig: Configuration = { themes: {} };
+
+export default defaultConfig;
