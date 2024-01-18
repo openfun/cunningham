3d4236c7bdf01361534bc8dafa663e718562d35e
Nathan Vasse <nathan.vasse@gmail.com>
2024-01-18 14:38:12 +0100

    🏷️(react) add WithOptional type
    This type allows to make only specific attributes optionnal where
    the existing Partial<T> makes all the attributes optionnal. Its
    first use will be for Modals.

--- packages/react/src/types/index.ts
+++ packages/react/src/types/index.ts
@@ -5,3 +5,7 @@ export type PartialNested<T> = {
 export type PartialExtendableNested<T> = {
   [K in keyof T]?: T[K] extends object ? PartialExtendableNested<T[K]> : T[K];
 } & Record<PropertyKey, any>;
+
+// From https://github.com/Microsoft/TypeScript/issues/25760#issuecomment-405931434.
+export type WithOptional<T, K extends keyof T> = Omit<T, K> &
+  Partial<Pick<T, K>>;
