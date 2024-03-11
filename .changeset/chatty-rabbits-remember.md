28547bbc6ee0af456e3fc3732bdb5a19eb22352d
Nathan Vasse <nathan.vasse@gmail.com>
2024-03-11 16:00:01 +0100

    🐛(react) add warning when onSortModelChange is missing
    We encountered the case were a user reported that the column were
    making cursor pointer but clicking on it was doing nothing. It was
    caused by the fact that onSortModelChange was not provided. So in
    this case we trigger a warning only if enableSorting is true on one
    column at least.

---
+++ .changeset/chatty-rabbits-remember.md
@@ -0,0 +1,5 @@
+---
+"@openfun/cunningham-react": patch
+---
+
+add warning on DataGrid when sortModel is missing
--- packages/react/src/components/DataGrid/DataList.tsx
+++ packages/react/src/components/DataGrid/DataList.tsx
@@ -2,5 +2,15 @@ import React from "react";
 import { BaseProps, DataGrid, Row } from ":/components/DataGrid/index";
 
 export const DataList = <T extends Row>({ rows, ...props }: BaseProps<T>) => {
-  return <DataGrid {...props} displayHeader={false} rows={rows} />;
+  return (
+    <DataGrid
+      {...props}
+      displayHeader={false}
+      rows={rows}
+      columns={props.columns.map((column) => ({
+        ...column,
+        enableSorting: false,
+      }))}
+    />
+  );
 };
