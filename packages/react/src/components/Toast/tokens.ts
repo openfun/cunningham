6fd2a0eff6cfc91ab43f9d5d90ea85d556e50f1f
Nathan Panchout <nathanpanchout@hotmail.com>
2025-08-22 10:16:01 +0200

    🎨(react) enhance Toast component styles
    update Toast token and css files with new css variables introduce
    with the new tokens architectures

--- packages/react/src/components/Toast/tokens.ts
+++ packages/react/src/components/Toast/tokens.ts
@@ -4,9 +4,9 @@ export const tokens = (defaults: DefaultTokens) => {
   return {
     "slide-in-duration": "1000ms",
     "slide-out-duration": "300ms",
-    "background-color": defaults.theme.colors["greyscale-100"],
-    color: defaults.theme.colors["greyscale-900"],
-    "font-weight": defaults.theme.font.weights.regular,
+    "background-color": defaults.contextuals.background.neutral.tertiary,
+    color: defaults.contextuals.content.neutral.primary,
+    "font-weight": defaults.globals.font.weights.regular,
     "icon-size": "19px",
     "progress-bar-height": "3px",
   };
