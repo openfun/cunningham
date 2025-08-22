859c5fc24fc002376f6aaabb7bddaa130b54b087
Nathan Panchout <nathanpanchout@hotmail.com>
2025-08-22 10:16:14 +0200

    🎨(react) enhance Tooltip component styles
    update Tooltip token and css files with new css variables introduce
    with the new tokens architectures

--- packages/react/src/components/Tooltip/tokens.ts
+++ packages/react/src/components/Tooltip/tokens.ts
@@ -3,9 +3,9 @@ import { DefaultTokens } from "@openfun/cunningham-tokens";
 export const tokens = (defaults: DefaultTokens) => {
   return {
     "border-radius": "0.5rem",
-    "background-color": defaults.theme.colors["greyscale-900"],
-    color: defaults.theme.colors["greyscale-000"],
-    "font-size": defaults.theme.font.sizes.s,
+    "background-color": defaults.contextuals.background.surface.primary,
+    color: defaults.contextuals.content.contextual.primary,
+    "font-size": defaults.globals.font.sizes.s,
     padding: "1rem",
     "max-width": "150px",
   };
